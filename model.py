8c16363ec6b21f8bd57b32536d20056677d23878
BinuShaw <binushaw66@gmail.com>
2023-11-20 11:47:35 +0530

    added 3 ml model

--- model.py
+++ model.py
@@ -1,1 +1,10 @@
 print("adding all ml related code here")
+
+def model1():
+    print("adding model one")
+    
+def model2():
+    print("adding model two")
+    
+def model3():
+    print("adding model three")
